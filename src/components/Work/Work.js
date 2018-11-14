cf98fea051f62956d587222ccd20f7139a37d8da
Kendra Kyndberg <kendrak922@gmail.com>
2018-11-14 14:43:46 -0600

    updated job description

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -12,7 +12,7 @@ const Work = () => (
           <p className="job-title">Portfolio Management Associate</p>
           <p className="dates-worked">June 2018 - Present</p>
           <p className="job-description">
-            I don't do anything at this jobb. It's a fake job. #thx Carlson.
+          In my current role I build and nurture relationships with internal team members and clients with proactive communication. I develop efficient internal communication by creating and distributing agendas in advance and meeting summaries afterwards. Furthermore, my job involves producing and maintaining project schedules and using program management tools to move projects through the agency in accordance with established processes. I also regularly seek information to understand campaign objectives and client direction while relaying these changes to the team at large.
           </p>
         </div>
       </div>
@@ -25,7 +25,8 @@ const Work = () => (
           <p className="sub-dates">July 2017 - Nov 2017</p>
           <p className="dates-worked">July 2017 - Dec 2017</p>
           <p className="job-description">
-          Created marketing material for 7 different social media platforms by writing copy and formatting content and graphics. Processed orders and coordinated transportation of freight to streamline shipping and transportation processes through 9 different carriers.
+          I was responsible for managing marketing materials and content across seven different social media platforms. This task required writing copy and formating graphics. 
+My additional position included processing orders and streamlining shipping and transportation logistics on a large scale.
           </p>
         </div>
       </div>
@@ -35,7 +36,7 @@ const Work = () => (
           <p className="job-title">Business Assistant</p>
           <p className="dates-worked">August 2016 - July 2017</p>
           <p className="job-description">
-          Produced client presentations and assisted owner at client meetings, following-up with errands around town. Displayed consistent professionalism and communicated at high levels with clients, vendors, and contractors. Contributed to company growth through office management and updating social media outlets.
+          Design Innovations is an Interior Design Firm whose operations span the entire country. Here, I was instrumental in maintaining client relationships by creating presentations and working with the company owner during meetings with clients. I displayed consistent professionalism and communicated at high levels with clients, vendors, and contractors. Furthermore, I contributed to company growth through office management and updating social media outlets.
           </p>
         </div>
       </div>
@@ -45,7 +46,7 @@ const Work = () => (
           <p className="job-title">Publicity & Promotions Intern</p>
           <p className="dates-worked">May 2016 - Jan 2017</p>
           <p className="job-description">
-          Built and executed successful promotional campaigns for upcoming films, specifically for Walt Disney Studios. Established relationships with third-party partners for collaboration on promotional events.
+          In this position I built and executed promotional campaigns for upcoming films, specifically Walt Disney Studios. I managed a continuous stream of projects by creating and maintaining relationships with third-party partners for collaboration on promotional events.
           </p>
         </div>
       </div>
