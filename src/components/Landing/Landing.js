1231966d38a1c3795727538b61c3d74115950ab7
Kendra Kyndberg <kendrak922@gmail.com>
2018-11-30 09:20:58 -0600

    changed landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -4,7 +4,7 @@ import rose from "./natalierose.png";
 const Landing = () => (
   <div className="page" id="landing-pg">
     <img className="roselogo" id='rose-logo' src={rose} alt="Rose"></img>
-    <h6>hey, I'm</h6>
+    <h6>hi, I'm</h6>
     <h1>Natalie</h1>
     <a href="#about-pg"><i className="fas fa-angle-down"></i></a>
   </div>
